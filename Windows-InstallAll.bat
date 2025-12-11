0557f51a54f0c66557410bd8a32bee76c12235f2
raulacor <raul.acorsi@gmail.com>
2025-12-11 13:41:35 -0300

    Add: Execution Command

--- Windows-InstallAll.bat
+++ Windows-InstallAll.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
 :: set batch file script path as default working path
 CD /d %~dp0
-ECHO 
+ECHO ================================================================================= 
+
+    :: This executes al files in folders as well as subfolders silently, one at a time and in theri own respective order.
+    FOR /r "." %%a in (*.exe) do "%%~fa" -s
+
+    
