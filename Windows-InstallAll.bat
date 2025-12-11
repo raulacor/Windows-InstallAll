3bc98af08cf3650db769f374633ee7080c58c6aa
raulacor <raul.acorsi@gmail.com>
2025-12-11 13:43:22 -0300

    Add: Simple tips

--- Windows-InstallAll.bat
+++ Windows-InstallAll.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
-:: set batch file script path as default working path
+:: Sets batch file script path as default working path
 CD /d %~dp0
 ECHO ================================================================================= 
 
     :: This executes al files in folders as well as subfolders silently, one at a time and in theri own respective order.
     FOR /r "." %%a in (*.exe) do "%%~fa" -s
 
-    
+    ECHO Complete. 
+    ECHO Remember to reboot if needed
+    ECHO Monitor Task Manager Processes to ensure installations complete
+        TIMEOUT 120
